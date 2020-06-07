89c95c423dcefa866d2b2f264a0f3fcaaaf896fb
alfredangeline <alfredangeline@hotmail.com>
2020-06-06 20:43:51 -0700

    Add files via upload

---
+++ ip/microphone/microphone.srcs/sources_1/new/microphone.v
@@ -0,0 +1,107 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/06/2020 04:44:12 PM
+// Design Name: 
+// Module Name: microphone
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module microphone
+#(
+    parameter DATA_WIDTH = 16,
+    parameter AUD_SAMPLE_FREQ_HZ = 44100,
+    parameter SYSCLK_FREQ_MHZ    = 100,
+    parameter PACKET_SIZE = 4096
+)
+(
+    input s_aclk,
+    input s_aresetn,
+    input EN,
+    input pdm_data,
+    output pdm_lrsel,
+    output reg pdm_clk,
+
+
+    output reg [DATA_WIDTH-1:0] m_axis_tdata,
+    output	m_axis_tlast,  
+    input	m_axis_tready,
+    output	m_axis_tvalid
+    );
+    
+    localparam bit_freq = (SYSCLK_FREQ_MHZ * 1000000)/ (2 * AUD_SAMPLE_FREQ_HZ * DATA_WIDTH);
+    integer clock_counter, bit_counter, packetcounter;
+    reg[DATA_WIDTH-1:0] temp_data;
+    reg pdm_clk_rising;
+    
+    assign pdm_lrsel = 1'b0; // select left channel
+    
+        // generate the pdm clock
+    always @(posedge s_aclk or negedge s_aresetn) begin
+        if(~s_aresetn | EN) begin
+            clock_counter <= 0;
+            pdm_clk <= 1'b0;
+            pdm_clk_rising <= 1'b0;
+         end
+         else if(clock_counter == bit_freq) begin
+            clock_counter <= 0;
+            if(pdm_clk == 1'b0)
+                pdm_clk_rising <= 1'b1;
+            pdm_clk <= ~pdm_clk;
+         end
+         else begin
+            clock_counter <= clock_counter + 1; 
+            pdm_clk <= pdm_clk; 
+            pdm_clk_rising <= 1'b0;  
+         end  
+    end
+    
+    
+    // deserialize the pdm_data and Count the number of sampled bits
+    always @(posedge s_aclk or negedge s_aresetn) begin
+        if(~s_aresetn | EN) begin
+            temp_data <= 16'd0;
+            bit_counter <= 0;
+         end
+         else if (pdm_clk_rising) begin
+            temp_data[DATA_WIDTH - bit_counter - 1] <= pdm_data; 
+            if(bit_counter == DATA_WIDTH - 1)
+                    bit_counter <= 0;
+             else
+                bit_counter  <= bit_counter + 1;
+         end  
+    end
+    
+    assign m_axis_tvalid = (bit_counter == 0);
+
+    // assign data_ready signal and load the output data
+    always @(posedge s_aclk or negedge s_aresetn) begin
+        if(~s_aresetn | EN) begin
+             m_axis_tdata <= 0;
+             packetcounter <= 0;
+         end
+         else if(m_axis_tvalid  && m_axis_tready) begin
+             m_axis_tdata <= temp_data;
+             if(packetcounter == PACKET_SIZE -1)
+                packetcounter <= 0;
+             else
+                packetcounter <= packetcounter + 1;
+         end 
+    end
+    
+    assign m_axis_tlast = (packetcounter == PACKET_SIZE-1);
+    
+endmodule
---
+++ ip/speaker/speaker.srcs/sources_1/new/speaker.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/06/2020 04:47:18 PM
+// Design Name: 
+// Module Name: speaker
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module speaker
+#(
+    parameter DATA_WIDTH = 16,
+    parameter AUD_SAMPLE_FREQ_HZ = 44100,
+    parameter SYSCLK_FREQ_MHZ    = 100
+)
+(
+    input s_aclk,
+    input s_aresetn,
+    input EN,
+    output PWM_audio,
+    output PDM_sd,
+
+    input [DATA_WIDTH-1:0] s_axis_tdata,
+    input	s_axis_tlast,  //unused
+    output	s_axis_tready,
+    input	s_axis_tvalid
+    
+    );
+    
+    localparam bit_freq = (SYSCLK_FREQ_MHZ * 1000000)/ (AUD_SAMPLE_FREQ_HZ * DATA_WIDTH);
+    
+    integer clock_counter, bit_counter;
+    reg[DATA_WIDTH-1:0] temp_data;
+    reg pdm_clk_rising;
+    
+    assign PDM_sd = 1'b1;
+    assign s_axis_tready = (bit_counter == 0);
+    
+    // Count the number of sampled bits
+    always @(posedge s_aclk or negedge s_aresetn)
+        if(~s_aresetn | EN)
+            bit_counter <= 0;
+        else if (pdm_clk_rising)
+            if(bit_counter == DATA_WIDTH-1)
+                bit_counter <= 16'd0;
+            else
+                bit_counter  <= bit_counter + 1;
+ 
+                  
+    // serialize the pdm_data
+    always @(posedge s_aclk or negedge s_aresetn)
+        if(~s_aresetn | EN)
+            temp_data <= 16'd0;
+        else if(s_axis_tready && s_axis_tvalid)
+             temp_data <= s_axis_tdata;
+             
+     // assign the serialized PWM_audio signal         
+     assign PWM_audio = temp_data[DATA_WIDTH - bit_counter - 1];
+                
+                
+     // generate the pdm clock
+    always @(posedge s_aclk or negedge s_aresetn) begin
+        if(~s_aresetn | EN) begin
+            clock_counter <= 0;
+            pdm_clk_rising <= 1'b0;
+         end
+         else if(clock_counter == bit_freq) begin
+            clock_counter <= 0;
+            pdm_clk_rising <= 1'b1;
+         end
+         else begin
+            clock_counter <= clock_counter + 1; 
+            pdm_clk_rising <= 1'b0;   
+         end  
+    end
+    
+    
+endmodule
